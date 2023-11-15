d1d4851d75774312dae13c89626bb297ab3484fc
achmdz <shikimadz@gmail.com>
2023-11-15 21:12:30 +0700

    update foto

--- resources/views/BukuTahunan/index.blade.php
+++ resources/views/BukuTahunan/index.blade.php
@@ -10,7 +10,7 @@
   <div class="col mt-3">
     <div class="card" style="height:520px;border-radius: 15px;">
       <div class="card-body p-3">
-        <center><img src=" google.jpg" class="w-75"></center>
+        <center><img src="{{$item['image'];}}" class="w-75"></center>
         <h5 class="card-title mb-1 mt-4">{{$item["name"]}}</h5>
         <p class="text-muted pb-2" style="border-bottom: 2px solid #eee;">{{$item["class"]}}.</p>
         <p class="card-text mb-3">{{$item["quotes"]}}.</p>
