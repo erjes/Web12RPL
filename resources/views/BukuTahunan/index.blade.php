ecfa5aac510ef6cbc653c266f55f261f6abf098a
achmdz <shikimadz@gmail.com>
2023-11-15 21:12:30 +0700

    update foto

--- resources/views/BukuTahunan/index.blade.php
+++ resources/views/BukuTahunan/index.blade.php
@@ -1,5 +1,6 @@
 @section("content")
 <!-- KONTEN -->
+<<<<<<< HEAD
 <h1>Photo Groups</h1>
 @foreach ($groups as $group)
 <div class="group" class="d-flex justify-content-start">
@@ -20,3 +21,36 @@
     @endforeach
 </body>
 </html>
+=======
+<div class="column">
+    @foreach($groups["students"] as $item)
+<<<<<<< HEAD
+  <div class="row mb-2">
+    <div class="card mt-2">
+      <img src="{{$item["image"]}}" alt="" style="width:100%">
+      <div class="container">
+        <h2>{{$item["name"]}}</h2>
+        <p>{{$item["class"]}}</p>
+        <p>{{$item["quotes"]}}</p>
+        <p><button class="button">Contact</button></p>
+=======
+
+  <div class="col mt-3">
+    <div class="card" style="height:520px;border-radius: 15px;">
+      <div class="card-body p-3">
+        <center><img src="{{$item['image'];}}" class="w-75"></center>
+        <h5 class="card-title mb-1 mt-4">{{$item["name"]}}</h5>
+        <p class="text-muted pb-2" style="border-bottom: 2px solid #eee;">{{$item["class"]}}.</p>
+        <p class="card-text mb-3">{{$item["quotes"]}}.</p>
+
+>>>>>>> d1d4851 (update foto)
+      </div>
+    </div>
+  </div>
+@endforeach
+  
+</div>
+<!-- KONTEN -->
+@endsection
+@include("layouts.header")
+>>>>>>> 63f6ae2 (update foto)
