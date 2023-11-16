75ba5e61a4d0a2d7af61f9f09847a0f13a2fb21d
Azhar Rudin <azharrudin0905@gmail.com>
2023-11-16 07:22:39 +0700

    fix conflict (completed)

--- resources/views/BukuTahunan/index.blade.php
+++ resources/views/BukuTahunan/index.blade.php
@@ -1,41 +1,34 @@
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 @section("content")
+
 <!-- KONTEN -->
-<h1>Photo Groups</h1>
-@foreach ($groups as $group)
-<div class="group" class="d-flex justify-content-start">
-    <h2>{{ $group['name'] }}</h2>
-    <img src="{{ $group['photo'] }}" alt="Photo of group {{ $group['name'] }}">
+<div class="column" id="c">
+<div class="row">
 
-    @foreach ($groups as $group)
-        <div class="group">
-            <h2>{{ $group['name'] }}</h2>
-            <img src="{{ $group['photo'] }}" alt="Photo of group {{ $group['name'] }}">
-
-            <ul>
-                @foreach ($group['students'] as $student)
-                    <li>{{ $student['name'] }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endforeach
-</body>
-</html>
-<div class="column">
     @foreach($groups["students"] as $item)
 
   <div class="col mt-3">
     <div class="card" style="height:520px;border-radius: 15px;">
       <div class="card-body p-3">
-        <center><img src="{{$item['image'];}}" class="w-75"></center>
+        <center><img src="{{$item['image']}}" class="w-75"></center>
         <h5 class="card-title mb-1 mt-4">{{$item["name"]}}</h5>
         <p class="text-muted pb-2" style="border-bottom: 2px solid #eee;">{{$item["class"]}}.</p>
         <p class="card-text mb-3">{{$item["quotes"]}}.</p>
 
       </div>
+      
     </div>
   </div>
 @endforeach
-  
+
+</div>
+  <style>
+    #c {
+      background-image: url('image/background2.jpg');
+      background-repeat: no-repeat;
+      background-size:cover;
+    }
+  </style>
 </div>
 <!-- KONTEN -->
 @endsection
